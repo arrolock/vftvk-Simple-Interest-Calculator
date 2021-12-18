e61a9dce6c9baafa29f7c2bcb31ec3333f0aef22
Andrew Lim <alimce@gmail.com>
2021-12-18 20:40:15 +0000

    updated files

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var intereest = principal * years * rate/100;
+    var interest = principal * years * rate/100;
     var year = new Date().getFullYear + parseInt(years);
-    p = document.getElementById("principal").value;
-    
+    document.getElementById("result").innerHTML = "If you deposit " + principal + ",<br>" 
+                                                + "at an interest rate of " + rate + ".<br>"
+                                                + "You will receive an amount of " + interest + ",<br>"
+                                                + "in the year " + year    
 }
 
 function updateRate() 
