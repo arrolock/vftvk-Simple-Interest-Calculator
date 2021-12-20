97ed89035fcd5c044acf39a98fd0d545e5e7f224
Andrew Lim <alimce@gmail.com>
2021-12-20 14:25:05 -0500

    final update

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-    if (document.getElementById("principal").validity.rangeUnderflow) {
-      document.getElementById("principal_msg").innerHTML = "Value is too small";
+    if (document.getElementById("principal").validity.rangeUnderflow || document.getElementById("principal")) {
+      alert("Enter a positive number.");
+      document.getElementById("principal").focus();
     } else {
       var rate = document.getElementById("rate").value;
       var years = document.getElementById("years").value;
       var interest = principal * years * rate/100;
       var year = new Date().getFullYear() + parseInt(years);
-      document.getElementById("result").innerHTML = `<br><p>If you deposit ${principal}
-                                                     at an interest rate of ${rate},
-                                                     you will receive an amount of ${interest}
-                                                     in the year ${year}</p>`;
+      document.getElementById("result").innerHTML = `<br><p>If you deposit <mark>${principal}</mark>
+                                                     at an interest rate of <mark>${rate}%</mark>.
+                                                     You will receive an amount of <mark>${interest}</mark>
+                                                     in the year <mark>${year}</mark></p>`;
     }
 }
 
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = rateval + "%";
 }
